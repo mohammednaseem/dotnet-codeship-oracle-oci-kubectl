9b5fcc217b2dea4bcf0511b7291c0b0b705f8c1a
m.naseem <m.naseem@outlook.com>
2020-04-28 17:03:29 +0530

    adding webapi

---
+++ webapi/ExHealthCheck.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Insurance_v1
+{
+    public class ExHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+                                                        HealthCheckContext context,
+                                                        CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var healthCheckResultHealthy = true;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("An unhealthy result."));
+        }
+    }
+}
---
+++ webapi/WeatherForecast.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Insurance_v1
+{
+    public class WeatherForecast
+    {         
+        public string Summary { get; set; }
+    }
+}
